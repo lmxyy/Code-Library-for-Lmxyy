cf63210638e8c374c7bf2b7c44a5cd6d0e2625b9
lmxyy <664789531@qq.com>
2017-08-02 22:47:51 +0800

    modify isap.cpp

--- source/图论算法/isap.cpp
+++ source/图论算法/isap.cpp
@@ -1,22 +1,23 @@
 //isap:有毒
+
 inline void bfs()
 {
-    queue <int> team; memcpy(cur,side,4*(n+4));
+    queue <int> team; memcpy(cur,side,4*(N+1));
     team.push(sink); d[sink] = 1; in[sink] = true;
     while (!team.empty())
     {
         int now = team.front(); team.pop(); nd[d[now]]++;
-        for (int i = side[now];i;i = next[i])
+        for (int i = side[now];i;i = nxt[i])
             if (cap[i^1] && !in[toit[i]])
                 in[toit[i]] = true,d[toit[i]] = d[now]+1,team.push(toit[i]);
     }
+	for (int i = 1;i <= N;++i) if (!in[i]) nd[d[i] = N+1]++;
 }
-
 inline int isap()
 {
-    bfs();
     int res = 0,now = source,ca = inf;
-    while (d[source] <= n)
+	bfs(); 
+    while (d[source] <= N)
     {
         if (now == sink)
         {
@@ -27,18 +28,18 @@ inline int isap()
             }
             res += ca; ca = inf;
         }
-        bool flag = false;
-        for (int i = cur[now];i;i = next[i])
-            if (cap[i] && d[toit[i]] == d[now] - 1)
+        bool flag = false; arr[now] = ca;
+        for (int i = cur[now];i;i = nxt[i])
+            if (cap[i]&&d[toit[i]]==d[now]-1)
             {
                 cur[now] = pre[toit[i]] = i; ca = min(ca,cap[i]);
                 now = toit[i]; flag = true; break;
             }
-        if (flag) continue; if (!--nd[d[now]]) break; int arg = n;
-        for (int i = side[now];i;i = next[i])
+        if (flag) continue; if (!--nd[d[now]]) break; int arg = N;
+        for (int i = side[now];i;i = nxt[i])
             if (cap[i]&&d[toit[i]] < arg) arg = d[toit[i]];
-        ++nd[d[now] = arg + 1]; cur[now] = side[now];
-        if (now != source) now = toit[pre[now] ^ 1];
-    }
+        ++nd[d[now]=arg+1]; cur[now] = side[now];
+        if (now != source) ca = arr[now = toit[pre[now]^1]];
+    }	
     return res;
 }
