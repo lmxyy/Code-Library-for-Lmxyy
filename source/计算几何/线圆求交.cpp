d11c1ed06d3c74460f6338987ddc440d46795f39
lmxyy <664789531@qq.com>
2017-08-17 17:22:15 +0800

    balabala

--- source/计算几何/线圆求交.cpp
+++ source/计算几何/线圆求交.cpp
@@ -1,6 +1,6 @@
 //a b直线两点，o圆心
 //若a b为线段，则0 <= t1,t2 <= 1
-inline void cp(const Node &a,const Node &b,const Node &o,double r,Node *ret,int &num)
+inline void crosspoint(const Point &a,const Point &b,const Point &o,double r,Point *ret,int &num)
 {
 	double X0 = o.x,Y0 = o.y;
 	double X1 = a.x,Y1 = a.y;
@@ -15,7 +15,7 @@ inline void cp(const Node &a,const Node &b,const Node &o,double r,Node *ret,int 
 	{
 		double t1 = (-B-sqrt(delta))/(2*A);
 		double t2 = (-B+sqrt(delta))/(2*A);
-		ret[++num] = Node(X1+t1*dx,Y1+t1*dy);
-		ret[++num] = Node(X1+t2*dx,Y1+t2*dy);
+		ret[++num] = Point(X1+t1*dx,Y1+t1*dy);
+		ret[++num] = Point(X1+t2*dx,Y1+t2*dy);
 	}
 }
