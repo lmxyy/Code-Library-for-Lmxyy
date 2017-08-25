c5a05d2bcc841ccbb742d3f31e34fd203066fd1d
lmxyy <664789531@qq.com>
2017-08-25 19:32:55 +0800

    jkldfjakdslf

--- source/图论算法/朱刘算法.cpp
+++ source/图论算法/朱刘算法.cpp
@@ -1,92 +1,66 @@
-//朱刘算法
-#include<iostream>
-#include<cstdio>
-#include<cstdlib>
-using namespace std;
 
-#define maxn (25010)
-#define inf (0x3fffffff)
-int n,m,tot,nn,need[55],id[maxn],pre[maxn],vis[maxn],change[maxn];
-double ans,in[maxn]; double wf[55]; bool exist[maxn];
-struct node { int p1,p2; double cost; }edge[3010];
-
-inline void read(int a,int b,double Cost)
-{ tot++; edge[tot].p1 = a; edge[tot].p2 = b; edge[tot].cost = Cost; }
-
-inline void init()
-{ for (int i = 0;i<=nn;i++) in[i] = inf,id[i] = -1,vis[i] = -1; }
-inline void zhuliu(int root)
+struct Directed_MT
 {
-	while (true)
+	struct Edge
 	{
-		init();
-		for (int i = 1;i<=tot;i++)
-		{
-			int u = edge[i].p1,v = edge[i].p2;
-			if (u == v||edge[i].cost>in[v]) continue;
-			in[v] = edge[i].cost,pre[v] = u;
-		}
-		in[root] = 0; int cir = 0;
-		for (int i = 0;i<=nn;i++) ans += in[i];
-		for (int i = 0;i<=nn;i++)
-		{
-			if (in[i] == inf) continue;
-			if (vis[i] == -1)
-			{
-				int t = i;
-				while (vis[t] == -1 && id[t] == -1 && t != root)
-					vis[t] = i,t = pre[t];
-				if (vis[t]!=i||t==root || id[t] != -1) continue;
-				for (int j = pre[t];j!=t;j = pre[j]) id[j] = cir;
-				id[t] = cir++;
-			}
-		}
-		if (!cir) break;
-		for (int i = 0;i<=nn;i++)
-			if (id[i] == -1 && in[i] != inf) id[i] = cir++;
-		for (int i = 1;i<=tot;i++)
-		{
-			int u = edge[i].p1,v = edge[i].p2;
-			edge[i].p1 = id[u]; edge[i].p2 = id[v];
-			if (id[u] != id[v]) edge[i].cost -= in[v];
-		}
-		nn = cir - 1;
-		root = id[root];
-	}
-}
+		int u,v,w;
+		inline Edge() = default;
+		inline Edge(int _u,int _v,int _w):u(_u),v(_v),w(_w) {}
+	};
+	int nn,mm,n,m,vis[maxn],pre[maxn],id[maxn],in[maxn]; Edge edges[maxn],bac[maxn];
 
-int main()
-{
-    freopen("shopping.in","r",stdin);
-    freopen("shopping.out","w",stdout);
-    scanf("%d",&n);
-    for (int i = 1;i<=n;i++)	
-    {
-		double a; int b;
-		scanf("%lf %d",&a,&b);
-		if (b)
+	inline void init(int _n) { n = _n; m = 0; }
+	inline void AddEdge(int u,int v,int w) { edges[m++] = Edge(u,v,w); }
+	inline int work(int root)
+	{
+		int ret = 0;
+		while (true)
 		{
-			nn++; need[nn] = b; change[i] = nn;
-			read(0,nn,a); wf[nn] = a;
+			// 初始化
+			for (int i = 0;i < n;++i) in[i] = inf+1;
+			for (int i = 0;i < m;++i)
+			{
+				int u = edges[i].u,v = edges[i].v;
+				// 找寻最小入边，删除自环
+				if (edges[i].w < in[v]&&u != v)
+					in[v] = edges[i].w,pre[v] = u;
+			}
+			// 如果没有最小入边，表示该点不连通，则最小树形图形成失败
+			for (int i = 0;i < n;++i)
+			{
+				if (i == root) continue;
+				if (in[i] == inf+1) return inf;
+			}
+			int cnt = 0; // 记录缩点
+			memset(id,-1,sizeof id); memset(vis,-1,sizeof vis);
+			in[root] = 0;
+			for (int i = 0;i < n;++i)
+			{
+				ret += in[i]; int v = i;
+				// 找寻自环
+				while (vis[v] != i&&id[v] == -1&&v != root)
+					vis[v] = i,v = pre[v];
+				if (v != root&&id[v] == -1)
+				{
+					// 这里不能从i开始找，因为i有可能不在自环内
+					for (int u = pre[v];u != v;u = pre[u]) id[u] = cnt;
+					id[v] = cnt++;
+				}
+			}
+			// 如果没有自环了，表示最小树形图成功了
+			if (!cnt) break;
+			// 找到那些不是自环的，重新给那些点进行标记
+			for (int i = 0;i < n;++i)
+				if (id[i] == -1) id[i] = cnt++;
+			for (int i = 0;i < m;++i)
+			{
+				int u = edges[i].u,v = edges[i].v;
+				edges[i].v = id[v]; edges[i].u = id[u];
+				if (id[u] != id[v]) edges[i].w -= in[v];
+			}
+			// 缩点完后，点的数量就变了
+			n = cnt; root = id[root];
 		}
-		if (!b) exist[i] = true;
+		return ret;
 	}
-    scanf("%d",&m);
-    for (int i = 1;i<=m;i++)
-    {
-		int b,c; double a;
-		scanf("%d%d%lf",&b,&c,&a);
-		if (!exist[b]&&!exist[c])
-		{
-			read(change[b],change[c],a);
-			wf[change[c]] = min(wf[change[c]],a);
-		}
-	}
-    zhuliu(0);
-    for (int i = 1 ; i <= n ; i ++)
-		if (need[change[i]]-1 > 0)
-			ans += wf[change[i]]*(need[change[i]]-1) ;
-    printf("%.2lf",ans);
-    fclose(stdin);fclose(stdout);
-    return 0;
-}
+}MT;
--- source/数值算法/数值积分.cpp
+++ source/数值算法/数值积分.cpp
@@ -1,12 +1,13 @@
-template <class T>
-inline double simpson(const T &f,double a,double b,int n)
+//self-adapt simpson
+inline long double simpson(long double l,long double r,long double mid,long double Cl,long double Cr,long double Cm)
 {
-	double h = (b-a)/n,ret = f(a)+f(b);
-	for (int i = 1;i < n;i += 2) ret += 4*f(a+i*h);
-	for (int i = 2;i < n;i += 2) ret += 2*f(a+i*h);
-	return ret*h/3;
+    long double tCl = calc((l+mid)/2),tCr = calc((mid+r)/2);
+	long double ans=(r-l)*(Cl+Cr+4*Cm)/6,lans=(mid-l)*(Cl+Cm+4*tCl)/6,rans=(r-mid)*(Cr+Cm+4*tCr)/6;
+    if (r-l <= 1e-3&&fabs(lans+rans-ans)<eps) return ans;
+	// if (dep > lim&&fabs(lans+rans-ans)<eps) return ans;
+    else return simpson(l,mid,(l+mid)/2,Cl,Cm,tCl)+simpson(mid,r,(mid+r)/2,Cm,Cr,tCr);
 }
-
+//romberg---to be verified
 template <class T>
 inline double romberg(const T &f,double a,double b,double eps = 1e-8)
 {
@@ -26,6 +27,6 @@ inline double romberg(const T &f,double a,double b,double eps = 1e-8)
 		}
 		t.push_back(cur); k *= 2; h /= 2; ++i;
 	}
-	while (fabs(last - cur) > esp);
+	while (fabs(last - cur) > eps);
 	return t.back();
 }
