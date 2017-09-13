c3c6c8c51b6f0d6f1bead882e47854491624308e
lmxyy <664789531@qq.com>
2017-09-13 18:47:21 +0800

    add lowerbound and upperbound in Splay.cpp

--- source/Data Structure/Splay.cpp
+++ source/Data Structure/Splay.cpp
@@ -12,12 +12,33 @@ inline int find(int rk)
 	return 0;
 }
 
+inline int upperbound(int x)
+{
+	int ret = 0;
+	for (int now = root;now;)
+	{
+		if (key[now] > x) ret = now,now = ch[now][0];
+		else now = ch[now][1];
+	}
+	return ret;
+}
+inline int lowerbound(int x)
+{
+	int ret = 0;
+	for (int now = root;now;)
+	{
+		if (key[now] >= x) ret = now,now = ch[now][0];
+		else now = ch[now][1];
+	}
+	return ret;
+}
+
 inline void rotate(int x)
 {
-	int y = fa[x],z = fa[y],l = ch[y][0] != x,r = l ^ 1;
-	if (z) ch[z][ch[z][0] != y] = x;
-	fa[x] = z; fa[y] = x; fa[ch[x][r]] = y;
-	ch[y][l] = ch[x][r]; ch[x][r] = y;
+	int y = fa[x],z = fa[y],l = ch[y][0] != x,r = l^1;
+	if (z) ch[z][ch[z][0] != y] = x; fa[x] = z;
+	if (ch[x][r]) fa[ch[x][r]] = y;
+	ch[y][l] = ch[x][r]; fa[y] = x; ch[x][r] = y;
 	update(y); update(x);
 }
 inline void splay(int x,int aim) //aim is x's father.
@@ -30,7 +51,7 @@ inline void splay(int x,int aim) //aim is x's father.
 		int y = fa[x],z = fa[y];
 		if (z != aim)
 		{
-			if ((ch[y][0] == x)^(ch[z][0]) == y) rotate(x);
+			if ((ch[y][0] == x)^(ch[z][0] == y)) rotate(x);
 			else rotate(y);
 		}
 		rotate(x);
