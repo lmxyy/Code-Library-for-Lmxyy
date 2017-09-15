f14602e3a575dac818e606bb7094f5141f9d5ca5
lmxyy <664789531@qq.com>
2017-09-15 20:18:53 +0800

    add Fast Input And Output.cpp

--- source/Numerical Algorithms/Fast Input.cpp
+++
@@ -1,8 +0,0 @@
-inline int gi()
-{
-	char ch; int ret = 0,f = 1;
-	do ch = getchar(); while (!(ch >= '0'&&ch <= '9')&&ch != '-');
-	if (ch == '-') f = -1,ch = getchar();
-	do ret = ret*10+ch-'0',ch = getchar(); while (ch >= '0'&&ch <= '9');
-	return ret*f;
-}
