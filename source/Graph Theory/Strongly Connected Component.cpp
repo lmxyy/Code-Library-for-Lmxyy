26212c6d9b7f3f8274728ac4afdac331dca2d98e
lmxyy <664789531@qq.com>
2017-10-13 19:26:34 +0800

    modified Strongly Connected Component.cpp

--- source/Graph Theory/Strongly Connected Component.cpp
+++ source/Graph Theory/Strongly Connected Component.cpp
@@ -1,30 +1,24 @@
-int dfn[maxn],low[maxn],timestamp; bool instk[maxn]; 
+int dfn[maxn],low[maxn],timestamp;
 stack <int> stk; vector <int> scc[maxn];
-inline void tarjan(int now)
+void tarjan(int now)
 {
 	dfn[now] = low[now] = ++timestamp;
-	stk.push(now); instk[now] = true;
+	stk.push(now);
 	for (int i = side[now];i;i = nxt[i])
 	{
 		if (!dfn[toit[i]])
 			tarjan(toit[i]),low[now] = min(low[now],low[toit[i]]);
-		else if (instk[toit[i]]) low[now] = min(low[now],dfn[toit[i]]);
+		else if (!bel[toit[i]]) low[now] = min(low[now],dfn[toit[i]]);
 	}
 	if (dfn[now] == low[now])
 	{
 		++tot;
 		while (stk.top() != now)
 		{
-			++tot;
-			while (stk.top() != now)
-			{
-				instk[stk.top()] = false;
-				scc[tot].push_back(stk.top());
-				bel[stk.top()] = tot; stk.pop();
-			}
-			instk[now] = false;
 			scc[tot].push_back(stk.top());
 			bel[stk.top()] = tot; stk.pop();
 		}
+		scc[tot].push_back(stk.top());
+		bel[stk.top()] = tot; stk.pop();
 	}
 }
