b2e33e31b9e04fbcf9957af1025e730963e4a3df
lmxyy <664789531@qq.com>
2017-10-26 12:45:57 +0800

    add Minimum-Cut-Stoer-Wagner

---
+++ source/Graph Theory/Stoer Wagner Algorithm.cpp
@@ -0,0 +1,38 @@
+int G[maxn][maxn],node[maxn],dis[maxn]; bool vis[maxn],visit[maxn];
+
+inline int solve(int n)
+{
+	if (n == 1) return inf;
+	int answer = inf;
+	for (int i = 0;i < n;++i) node[i] = i;
+	while (n > 1)
+	{
+		int mx = 1;
+		for (int i = 0;i < n;++i)
+		{
+			dis[node[i]] = G[node[0]][node[i]];
+			if (dis[node[i]] > dis[node[mx]]) mx = i;
+		}
+		int prev = 0;
+		memset(visit,false,sizeof visit);
+		visit[node[0]] = true;
+		for (int i = 1;i < n;++i)
+		{
+			if (i == n-1)
+			{
+				answer = min(answer,dis[node[mx]]);
+				for (int k = 0;k < n;++k)
+					G[node[k]][node[prev]] = (G[node[prev]][node[k]] += G[node[k]][node[mx]]);
+				node[mx] = node[--n];
+			}
+			visit[node[mx]] = true; prev = mx; mx = -1;
+			for (int j = 1;j < n;++j)
+				if (!visit[node[j]])
+				{
+					dis[node[j]] += G[node[prev]][node[j]];
+					if (mx == -1||dis[node[mx]] < dis[node[j]]) mx = j;
+				}
+		}
+	}
+	return answer;
+}
