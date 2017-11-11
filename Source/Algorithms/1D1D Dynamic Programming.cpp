995dbb89f54d999a6c6cd00bd698117d89db2f6a
lmxyy <664789531@qq.com>
2017-11-11 23:27:18 +0800

    ready for check the source

--- Source/Algorithms/1D1D Dynamic Programming.cpp
+++ Source/Algorithms/1D1D Dynamic Programming.cpp
@@ -4,13 +4,17 @@
 #include<cstdlib>
 using namespace std;
 
-#define limit (1e18)
-#define maxn 100010
-#define maxm 40
+const double limit = 1e18;
+const int maxn = 100010,maxm = 40;
 int N,L,P,pre[maxn],top;
 char s[maxm];
 long double f[maxn];
-struct node { int l,r,key; }stack[maxn];
+struct Node
+{
+	int l,r,key;
+	inline Node() = default;
+	inline Node(int _l,int _r,int _key):l(_l),r(_r),key(_key) {}
+}stk[maxn];
 
 inline long double qsm(int a,int b)
 {
@@ -30,10 +34,11 @@ inline int find(int a)
 	while (l <= r)
 	{
 		mid = (l + r) >> 1;
-		if (stack[mid].l<=a&&stack[mid].r>=a) return stack[mid].key;
-		if (a < stack[mid].l) r = mid - 1;
+		if (stk[mid].l<=a&&stk[mid].r>=a) return stk[mid].key;
+		if (a < stk[mid].l) r = mid - 1;
 		else l = mid + 1;
 	}
+	return -1;
 }
 
 inline void updata(int now)
@@ -41,28 +46,28 @@ inline void updata(int now)
 	int l = 1,r;
 	while (top)
 	{
-		if (calc(stack[top].l,stack[top].key) >= calc(stack[top].l,now))
+		if (calc(stk[top].l,stk[top].key) >= calc(stk[top].l,now))
 			--top;
 		else
 		{
-			l = stack[top].l,r = stack[top].r;
+			l = stk[top].l,r = stk[top].r;
 			while (l <= r)
 			{
 				int mid = (l + r) >> 1;
-				if (calc(mid,stack[top].key) >= calc(mid,now)) r = mid - 1;
+				if (calc(mid,stk[top].key) >= calc(mid,now)) r = mid - 1;
 				else l = mid + 1;
 			}
-			stack[top].r = r;
+			stk[top].r = r;
 			break;
 		}
 	}
-	if (l <= N) stack[++top] = (node){l,N,now};
+	if (l <= N) stk[++top] = Node(l,N,now);
 }
 
 inline void dp()
 {
 	f[0] = 0;
-	stack[top = 1] = (node) {1,N,0};
+	stk[top = 1] =  Node(1,N,0);
 	for (int i = 1;i <= N;++i)
 	{
 		int key = find(i);
@@ -73,8 +78,6 @@ inline void dp()
 
 int main()
 {
-	freopen("1563.in","r",stdin);
-	freopen("1563.out","w",stdout);
 	int T; scanf("%d",&T);
 	while (T--)
 	{
@@ -90,6 +93,5 @@ int main()
 		else printf("%.0Lf\n",f[N]);
 		printf("--------------------\n");
 	}
-	fclose(stdin); fclose(stdout);
 	return 0;
 }
