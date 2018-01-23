544b79f4cbc3af1d672550539fddf94a4b74655d
lmxyy <664789531@qq.com>
2018-01-23 23:03:40 +0800

    add Leftlist Tree

--- Source/Data Structure/Leftlist Tree.cpp
+++ Source/Data Structure/Leftlist Tree.cpp
@@ -1,28 +1,41 @@
-// It's correct, but it needs be rewritten.
+// Checked by bzoj 1455
+#include<algorithm>
+#include<cstring>
 #include<iostream>
 #include<cstdio>
 #include<cstdlib>
 using namespace std;
 
-#define maxn (600010)
-int N,M,root[maxn],size[maxn],v[maxn],dep[maxn],l[maxn],r[maxn],tot;
+const int maxn = 1000010;
+int N,score[maxn],father[maxn]; bool killed[maxn];
 
-inline int Merge(int x,int y)
-{
-	if (!x||!y) return x+y;
-	if (v[x]>v[y]) swap(x,y);
-	r[x] = Merge(r[x],y);
-	if (dep[l[x]] < dep[r[x]]) swap(l[x],r[x]);
-	dep[x] = dep[r[x]]+1;
-	return x;
-}
-inline int Init(int x) { v[++tot] = x; l[tot] = r[tot] = dep[tot] = 0; return tot;}
-inline int Insert(int x,int y) { return Merge(x,Init(y)); }
-inline int pop(int x) { return Merge(l[x],r[x]); }
+int find(int a) { if (father[a] != a) return father[a] = find(father[a]); return a; }
 
-inline int read()
+template < typename Type,class Compare = std::less<Type> >
+struct LeftlistTree
 {
-	char ch; int f = 1,ret = 0;
+	Compare cmp; int tot;
+	int dep[maxn],size[maxn]; Type v[maxn]; int lc[maxn],rc[maxn];
+
+	inline void init() { tot = 0; }
+	inline int merge(int x,int y)
+	{
+		if (!x||!y) return x+y;
+		if (cmp(v[y],v[x])) swap(x,y);
+		rc[x] = merge(rc[x],y);
+		if (dep[lc[x]] < dep[rc[x]]) swap(lc[x],rc[x]);
+		dep[x] = dep[rc[x]]+1;
+		return x;
+	}
+	inline int newnode(const Type &x) { ++tot; v[tot] = x; lc[tot] = rc[tot] = dep[tot] = 0; return tot; }
+	inline int insert(int x,const Type &y) { return merge(x,newnode(y)); }
+	inline int pop(int x) { return merge(lc[x],rc[x]); }
+};
+LeftlistTree <int> heap;
+
+inline int gi()
+{
+	char ch; int ret = 0,f = 1;
 	do ch = getchar(); while (!(ch >= '0'&&ch <= '9')&&ch != '-');
 	if (ch == '-') f = -1,ch = getchar();
 	do ret = ret*10+ch-'0',ch = getchar(); while (ch >= '0'&&ch <= '9');
@@ -31,31 +44,33 @@ inline int read()
 
 int main()
 {
-	freopen("1050.in","r",stdin);
-	freopen("1050.out","w",stdout);
-	scanf("%d %d",&N,&M);
-	for (int i = 1;i <= N;++i) root[i] = Init(read()),size[i] = 1;
-	while (M--)
+	freopen("1455.in","r",stdin);
+	N = gi(); heap.init();
+	for (int i = 1;i <= N;++i) father[i] = i,heap.newnode(gi());
+	for (int M = gi();M--;)
 	{
-		int opt = read();
-		if (!opt)
+		char c; do c = getchar(); while (c != 'M'&&c != 'K');
+		if (c == 'M')
 		{
-			int a = read()+1,b = read()+1;
-			if (size[b]) root[a] = Merge(root[a],root[b]);
-			size[a] += size[b]; size[b] = 0;
-		}
-		else if (opt == 1)
-		{
-			int a = read()+1;
-			if (!size[a]) puts("-1");
-			else printf("%d\n",v[root[a]]),root[a] = pop(root[a]),--size[a];
+			int x = gi(),y = gi();
+			if (killed[x]||killed[y]) continue;
+			x = find(x); y = find(y);
+			if (x == y) continue;
+			int t = heap.merge(x,y);
+			father[x] = father[y] = t; 
 		}
 		else
 		{
-			int a = read()+1; ++size[a];
-			root[a] = Insert(root[a],read());
+			int x = gi();
+			if (killed[x]) puts("0");
+			else
+			{
+				x = find(x); killed[x] = true;
+				printf("%d\n",heap.v[x]);
+				int t = heap.pop(x);
+				father[t] = t; father[x] = t;
+			}
 		}
 	}
-	fclose(stdin); fclose(stdout);
 	return 0;
 }
